93287e6eeb7fd7030314cc125f4163b254be6d78
TonyPurple <tony.vanoni@outlook.com>
2022-05-13 10:59:04 -0700

    useState for react quill data

--- src/components/editdocs.jsx
+++ src/components/editdocs.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 export default function EditDocs() {
     let params = useParams();
+    const [docsDesc, setDocsDesc] = useState('');
+    const getQuillData = (value) => {
+        setDocsDesc(value)
+    }
     return (
         <div>
             <h1>EditDocs</h1>
 
-            <ReactQuill />
+            <ReactQuill
+   value={docsDesc}
+   onChange={getQuillData}
+/>
         </div>
     )
 }
