a33dbb6a5cec0555b0fe05c76a0193b9a63a9274
TonyPurple <tony.vanoni@outlook.com>
2022-05-13 10:20:18 -0700

    add docs data

--- src/components/docs.jsx
+++ src/components/docs.jsx
@@ -4,8 +4,12 @@ import ModalComponent from './modal';
 export default function Docs({
     database
 }) {
+    const [title, setTitle] = useState('')
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
+    const addData = () => {
+        
+    }
     return (
         <div className='docs-main'>
             <h1>Docs Clone</h1>
@@ -20,6 +24,9 @@ export default function Docs({
             <ModalComponent
                 open={open}
                 setOpen={setOpen}
+                title={title}
+                setTitle={setTitle}
+                onClick={addData}
             />
         </div>
     )
