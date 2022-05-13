f233c890bf57558010811276bb5ecdc0bde0ecf2
TonyPurple <tony.vanoni@outlook.com>
2022-05-13 11:08:26 -0700

    updating document

--- src/components/editdocs.jsx
+++ src/components/editdocs.jsx
@@ -2,12 +2,36 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-export default function EditDocs() {
+import {
+    updateDoc,
+    collection,
+    doc,
+    onSnapshot
+} from 'firebase/firestore';
+export default function EditDocs({
+    database
+}) {
+    const collectionRef = collection(database, 'docsData')
     let params = useParams();
     const [docsDesc, setDocsDesc] = useState('');
     const getQuillData = (value) => {
         setDocsDesc(value)
     }
+    useEffect(() => {
+        const updateDocsData = setTimeout(() => {
+            const document = doc(collectionRef, params.id)
+            updateDoc(document, {
+                docsDesc: docsDesc
+            })
+            .then(() => {
+                alert('Saved')
+            })
+            .catch(() => {
+                alert('Cannot Save')
+            })
+        }, 1000)
+        return () => clearTimeout(updateDocsData)
+    }, [docsDesc])
     return (
         <div>
             <h1>EditDocs</h1>
